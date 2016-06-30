5d25a1d1e72622f701fe0b76817648bd04160963
Dimitri Vorona <vorona@in.tum.de>
2016-06-30 15:14:08 +0200

    Add optional range selector to date search field in mod_data

--- mod/data/field/date/field.class.php
+++ mod/data/field/date/field.class.php
@@ -75,30 +75,105 @@ class data_field_date extends data_field_base {
     //Enable the following three functions once core API issues have been addressed.
     function display_search_field($value=0) {
         $selectors = html_writer::select_time('days', 'f_'.$this->field->id.'_d', $value['timestamp'])
-           . html_writer::select_time('months', 'f_'.$this->field->id.'_m', $value['timestamp'])
-           . html_writer::select_time('years', 'f_'.$this->field->id.'_y', $value['timestamp']);
-        $datecheck = html_writer::checkbox('f_'.$this->field->id.'_z', 1, $value['usedate']);
-        $str = $selectors . ' ' . $datecheck . ' ' . get_string('usedate', 'data');
+                        . html_writer::select_time('months', 'f_'.$this->field->id.'_m', $value['timestamp'])
+                        . html_writer::select_time('years', 'f_'.$this->field->id.'_y', $value['timestamp']);
+        $selectors_from = html_writer::select_time('days', 'f_'.$this->field->id.'_d_from', $value['timestamp_from'])
+           . html_writer::select_time('months', 'f_'.$this->field->id.'_m_from', $value['timestamp_from'])
+           . html_writer::select_time('years', 'f_'.$this->field->id.'_y_from', $value['timestamp_from']);
+        $selectors_to = html_writer::select_time('days', 'f_'.$this->field->id.'_d_to', $value['timestamp_to'])
+                        . html_writer::select_time('months', 'f_'.$this->field->id.'_m_to', $value['timestamp_to'])
+                        . html_writer::select_time('years', 'f_'.$this->field->id.'_y_to', $value['timestamp_to']);
+        $range_mode = html_writer::checkbox('f_'.$this->field->id.'_range',
+                                            1,
+                                            $value['range_select'],
+                                            get_string('rangeselect', 'data'),
+                                            array('class' => 'range_select_checkbox'));
+        $datecheck = html_writer::checkbox('f_'.$this->field->id.'_z', 1, $value['usedate'], get_string('usedate', 'data'));
+        $str = $range_mode . ' ';
+        $str .= '<div class="exact_date_sel">' . $selectors . '</div>';
+        $str .= '<div class="range_date_sel">' . 'From: <br />' . $selectors_from . '<br />';
+        $str .= 'To: <br />' . $selectors_to . '</div>';
+        $str .= $datecheck;
 
         return $str;
     }
 
     function generate_sql($tablealias, $value) {
         global $DB;
-
         static $i=0;
         $i++;
-        $name = "df_date_$i";
         $varcharcontent = $DB->sql_compare_text("{$tablealias}.content");
-        return array(" ({$tablealias}.fieldid = {$this->field->id} AND $varcharcontent = :$name) ", array($name => $value['timestamp']));
+
+        if ($value['range_select']) {
+            $params = array();
+            $query = " ({$tablealias}.fieldid = {$this->field->id}";
+
+            if (array_key_exists('timestamp_from', $value)) {
+                $from = "df_date_from_$i";
+                $query .= " AND $varcharcontent >= :$from";
+                $params[$from] = $value['timestamp_from'];
+            }
+            if (array_key_exists('timestamp_to', $value)) {
+                $to = "df_date_to_$i";
+                $query .= " AND $varcharcontent <= :$to";
+                $params[$to] = $value['timestamp_to'];
+            }
+            $query .= ") ";
+            if (!empty($params)) {
+                return array($query, $params);
+            }
+            return array('', array());
+        } else {
+            $name = "df_date_$i";
+            return array(" ({$tablealias}.fieldid = {$this->field->id} AND $varcharcontent = :$name) ", array($name => $value['timestamp']));
+        }
     }
 
     function parse_search_field() {
+        $range_select = optional_param('f_'.$this->field->id.'_range', 0, PARAM_INT);
+        $usedate = optional_param('f_'.$this->field->id.'_z', 0, PARAM_INT);
+        if (!$usedate) return 0;
+
+        $data = array();
+        $day_from = optional_param('f_'.$this->field->id.'_d_from', 0, PARAM_INT);
+        $month_from = optional_param('f_'.$this->field->id.'_m_from', 0, PARAM_INT);
+        $year_from = optional_param('f_'.$this->field->id.'_y_from', 0, PARAM_INT);
+        $day_to = optional_param('f_'.$this->field->id.'_d_to', 0, PARAM_INT);
+        $month_to = optional_param('f_'.$this->field->id.'_m_to', 0, PARAM_INT);
+        $year_to = optional_param('f_'.$this->field->id.'_y_to', 0, PARAM_INT);
+
+        $calendartype = \core_calendar\type_factory::get_calendar_instance();
+        if (!empty($day_from) && !empty($month_from) && !empty($year_from)) {
+
+            $gregoriandate_from = $calendartype->convert_to_gregorian($year_from, $month_from, $day_from);
+
+
+            $data['timestamp_from'] = make_timestamp(
+                $gregoriandate_from['year'],
+                $gregoriandate_from['month'],
+                $gregoriandate_from['day'],
+                $gregoriandate_from['hour'],
+                $gregoriandate_from['minute'],
+                0,
+                0,
+                false);
+        }
+        if (!empty($day_to) && !empty($month_to) && !empty($year_to)) {
+            $gregoriandate_to = $calendartype->convert_to_gregorian($year_to, $month_to, $day_to);
+            $data['timestamp_to'] = make_timestamp(
+                $gregoriandate_to['year'],
+                $gregoriandate_to['month'],
+                $gregoriandate_to['day'],
+                $gregoriandate_to['hour'],
+                $gregoriandate_to['minute'],
+                0,
+                0,
+                false);
+        }
+
         $day   = optional_param('f_'.$this->field->id.'_d', 0, PARAM_INT);
         $month = optional_param('f_'.$this->field->id.'_m', 0, PARAM_INT);
         $year  = optional_param('f_'.$this->field->id.'_y', 0, PARAM_INT);
-        $usedate = optional_param('f_'.$this->field->id.'_z', 0, PARAM_INT);
-        $data = array();
         if (!empty($day) && !empty($month) && !empty($year) && $usedate == 1) {
             $calendartype = \core_calendar\type_factory::get_calendar_instance();
             $gregoriandate = $calendartype->convert_to_gregorian($year, $month, $day);
@@ -113,10 +188,11 @@ class data_field_date extends data_field_base {
                 0,
                 false);
             $data['usedate'] = 1;
-            return $data;
-        } else {
-            return 0;
         }
+        $data['usedate'] = 1;
+        $data['range_select'] = $range_select;
+
+        return $data;
     }
 
     function update_content($recordid, $value, $name='') {
